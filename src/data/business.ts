b0ea44d8fc234bc70d39087d80b3305af884b400
PartickPinace <minor.michal1@gmail.com>
2026-09-18 07:39:06 +0200

    Dodaj białe spacje niełamiące przy krótkich spójnikach i numerach w tekście

--- src/data/business.ts
+++ src/data/business.ts
@@ -4,9 +4,9 @@ export const business = {
   licenseNumber: '1376',
   foundedYear: 1995,
   nip: '687-110-55-73',
-  phone: '692 439 759',
+  phone: '692 439 759',
   phoneHref: 'tel:+48692439759',
-  phoneSecondary: '13 46 319 57',
+  phoneSecondary: '13 46 319 57',
   phoneSecondaryHref: 'tel:+48134631957',
   email: 'gilarski2@op.pl',
   street: 'ul. Matejki 22',
@@ -14,7 +14,7 @@ export const business = {
   city: 'Sanok',
   region: 'Podkarpackie',
   country: 'PL',
-  hours: 'pon.-sob. 08:00-20:00',
+  hours: 'pon.-sob. 08:00-20:00',
   mapsDirectionsUrl:
     'https://www.google.com/maps/dir/?api=1&destination=ul.+Matejki+22%2C+38-500+Sanok',
 } as const;
